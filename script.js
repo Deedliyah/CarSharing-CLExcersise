32b1da3833742439062b84df5ea626bd39668cfd
Deedliyah <38763304+Deedliyah@users.noreply.github.com>
2020-07-05 16:01:04 +0200

    nieudany layout z zajęć

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+const header = document.querySelector(".page-header")
+console.log(header);
+window.addEventListener("scroll", function(){
+    if (window.scrollY > 150) {
+        header.classList.add("page-header--sticky")
+        }    else {
+        header.classList.remove("page-header--sticky")
+        }
+    });
+});
